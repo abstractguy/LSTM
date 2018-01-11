4384da99cb51fcc142139ad5d525f303ef1e8955
Samuel Duclos <samuel@localhost.localdomain>
2018-01-11 01:47:12 -0500

    Yo man heyhey

--- C/new/matrix.c
+++ C/new/matrix.c
@@ -42,24 +42,119 @@ void matrix_for_each(long double (*f)(long double), matrix *matrix1) {
   }
 }
 
+matrix *matrix_map2(long double (*f)(long double, long double), matrix *matrix1, matrix *matrix2) {
+  matrix *matrix3 = make_matrix(matrix1->rows, matrix1->columns);
+  for (unsigned int row = 0; row < matrix1->rows; row++) {
+    for (unsigned int column = 0; column < matrix1->columns; column++) {
+      matrix3->matrix[row][column] = f(matrix1->matrix[row][column], matrix2->matrix[row][column]);
+    }
+  } return matrix3;
+}
+
 matrix *dot_product(matrix *matrix1, matrix *matrix2) {
   matrix *matrix3 = make_matrix(matrix1->columns, matrix2->rows);
   assert(matrix1->rows == matrix2->columns);
   matrix_for_each(zero, matrix3);
+
   for (unsigned int row1 = 0; row1 < matrix1->rows; row1++) {
     for (unsigned int column2 = 0; column2 < matrix2->columns; column2++) {
       for (unsigned int column1 = 0; column1 < matrix1->columns; column1++) {
         matrix3->matrix[row1][column2] += matrix1->matrix[row1][column1] * matrix2->matrix[column1][column2];
       }
     }
-  }   return matrix3;
+  } return matrix3;
+}
+
+matrix *matrix_copy_shape(matrix *matrix1) {
+  return make_matrix(matrix1->rows, matrix1->columns);
 }
 
 matrix *matrix_copy(matrix *matrix1) {
-  matrix *matrix2 = make_matrix(matrix1->rows, matrix1->columns);
+  matrix *matrix2 = matrix_copy_shape(matrix1);
+
   for (unsigned int row = 0; row < matrix1->rows; row++) {
     for (unsigned int column = 0; column < matrix1->columns; column++) {
       matrix2->matrix[row][column] = matrix1->matrix[row][column];
     }
   } return matrix2;
+}
+
+matrix *matrix_initialize(long double (*init)(long double), matrix *matrix1) {
+  matrix *matrix2 = matrix_copy(matrix1);
+  matrix_for_each(init, matrix2);
+  return matrix2;
+}
+
+matrix *broadcast_vertical(matrix *matrix1, matrix *matrix2) {
+  matrix *matrix3 = NULL;
+
+  if ((matrix1->rows == 1) && (matrix2->rows > 1)) {
+    matrix3 = make_matrix(matrix2->rows, matrix1->columns);
+
+    for (unsigned int row = 0; row < matrix2->rows; row++) {
+      for (unsigned int column = 0; column < matrix1->columns; column++) {
+        matrix3->matrix[row][column] = matrix1->matrix[0][column];
+      }
+    } matrix1 = destroy_matrix(matrix1);
+      return matrix3;
+  }   else return matrix1;
+}
+
+matrix *broadcast_horizontal(matrix *matrix1, matrix *matrix2) {
+  matrix *matrix3 = NULL;
+  if ((matrix1->columns == 1) && (matrix2->columns > 1)) {
+    matrix3 = make_matrix(matrix1->rows, matrix2->columns);
+    for (unsigned int row = 0; row < matrix1->rows; row++) {
+      for (unsigned int column = 0; column < matrix2->columns; column++) {
+        matrix3->matrix[row][column] = matrix1->matrix[row][0];
+      }
+    } matrix1 = destroy_matrix(matrix1);
+      return matrix3;
+  }   else return matrix1;
+}
+
+matrix *broadcast_function(long double (*f)(long double, long double), matrix *matrix1, matrix *matrix2) {
+  matrix *matrix3 = matrix1;
+  matrix *matrix4 = matrix2;
+  matrix *matrix5 = NULL;
+
+  //assert((matrix3->columns == 1) || (matrix4->columns == 1) || (matrix3->rows == 1) || (matrix4->rows == 1) || (matrix3->columns == matrix4->columns) || (matrix3->rows == matrix4->rows));
+
+  matrix3 = broadcast_horizontal(matrix3, matrix4);
+  matrix4 = broadcast_horizontal(matrix4, matrix3);
+  matrix3 = broadcast_vertical(matrix3, matrix4);
+  matrix4 = broadcast_vertical(matrix4, matrix3);
+
+  matrix5 = matrix_map2(f, matrix3, matrix4);
+
+  matrix3 = destroy_matrix(matrix3);
+  matrix4 = destroy_matrix(matrix4);
+
+  return matrix5;
+}
+
+matrix *fold(long double (*f)(long double, long double), long double (*init)(long double), unsigned int times, matrix *matrix1, ...) {
+  matrix *matrix2 = matrix_initialize(init, matrix1);
+  unsigned int time = times - 1;
+
+  va_list args;
+  va_start(args, matrix1);
+
+  matrix2 = broadcast_function(f, matrix2, matrix1);
+
+  for (unsigned int t = 0; t < time; t++) {
+    matrix2 = broadcast_function(f, matrix2, va_arg(args, matrix *));
+  }
+
+  va_end(args);
+
+  return matrix2;
+}
+
+long double sum(long double x, long double y) {
+  return x + y;
+}
+
+long double product(long double x, long double y) {
+  return x * y;
 }
--- C/new/matrix.h
+++ C/new/matrix.h
@@ -4,6 +4,7 @@
   #include <stdlib.h>
   #include <assert.h>
   #include <math.h>
+  #include <stdarg.h>
 
   #define NOT_USED(x) ((void)x)
 
@@ -19,6 +20,14 @@
   long double one(long double);
   void matrix_for_each(long double (*)(long double), matrix *);
   matrix *dot_product(matrix *, matrix *);
+  matrix *matrix_copy_shape(matrix *);
   matrix *matrix_copy(matrix *);
+  matrix *matrix_initialize(long double (*)(long double), matrix *);
+  matrix *broadcast_vertical(matrix *, matrix *);
+  matrix *broadcast_horizontal(matrix *, matrix *);
+  matrix *broadcast_function(long double (*)(long double, long double), matrix *, matrix *);
+  matrix *fold(long double (*)(long double, long double), long double (*)(long double), unsigned int, matrix *, ...);
+  long double sum(long double, long double);
+  long double product(long double, long double);
 
 #endif
