ed11cc0062f0ea5c6fd8630f94089162e4ae80b0
Samuel Duclos <samuel@localhost.localdomain>
2018-01-12 18:54:23 -0500

    Debugging...

--- C/new/tests.c
+++ C/new/tests.c
@@ -7,6 +7,8 @@ int main(int argc, char *argv[]) {
   // TEST 3a:
   matrix *matrix1 = make_matrix(4, 1);
   matrix *matrix2 = make_matrix(4, 1);
+  matrix *matrix3 = NULL;
+  matrix *matrix4 = NULL;
   matrix_for_each(zero, matrix1);
   matrix_for_each(one, matrix2);
 
@@ -17,22 +19,27 @@ int main(int argc, char *argv[]) {
   // TEST 2:
   //push(LSTM, At_iota, dot_product(first(LSTM, Xt_i), first(LSTM, Wi_iota)));
 
-  /* TEST 3:
-  push(LSTM, St_c, 
-    fold(add, zero, 2, 
-      fold(multiply, one, 2, 
-        matrix_tanh(first(LSTM, At_c)), 
-        first(LSTM, Bt_iota)), 
-      fold(multiply, one, 2, 
-        second(LSTM, St_c), 
-        first(LSTM, Bt_phi))));
-  */
+  // TEST 3:
+  matrix3 =
+    fold(multiply, one, 2, 
+      matrix_tanh(first(LSTM, At_c)), 
+      first(LSTM, Bt_iota));
+  matrix4 =
+    fold(multiply, one, 2, 
+      second(LSTM, St_c), 
+      first(LSTM, Bt_phi));
+  push(LSTM, St_c, fold(add, zero, 2, matrix3, matrix4));
 
   //TEST 1:
   print_LSTM(LSTM);
 
   // TEST 3a:
-  print_matrix(fold(add, zero, 2, matrix1, matrix2));
+  //matrix3 = fold(add, zero, 2, matrix1, matrix2);
+  //print_matrix(matrix3);
+
+  // TEST 3:
+  matrix3 = destroy_matrix(matrix3);
+  matrix4 = destroy_matrix(matrix4);
 
   LSTM = destroy_LSTM(LSTM);
   return 0;
