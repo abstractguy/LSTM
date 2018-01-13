c94ed0f6e845601123373a91564cc10728447805
Samuel Duclos <samuel@localhost.localdomain>
2018-01-13 11:04:31 -0500

    Debugging leak...

--- C/new/tests.c
+++ C/new/tests.c
@@ -4,14 +4,10 @@ int main(int argc, char *argv[]) {
   // TEST 1:
   LSTM_type *LSTM = make_LSTM(4, 1);
 
-  // TEST 3a:
-  matrix *matrix1 = make_matrix(4, 1);
-  matrix *matrix2 = make_matrix(4, 1);
+  // TEST 3:
+  matrix *matrix1 = NULL;
+  matrix *matrix2 = NULL;
   matrix *matrix3 = NULL;
-  matrix *matrix4 = NULL;
-  matrix *matrix5 = NULL;
-  matrix_for_each(zero, matrix1);
-  matrix_for_each(one, matrix2);
 
   // TEST 1:
   NOT_USED(argc);
@@ -21,31 +17,25 @@ int main(int argc, char *argv[]) {
   //push(LSTM, At_iota, dot_product(first(LSTM, Xt_i), first(LSTM, Wi_iota)));
 
   // TEST 3:
-  matrix3 =
+  matrix1 =
     fold(multiply, one, 2, 
       matrix_tanh(first(LSTM, At_c)), 
       first(LSTM, Bt_iota));
-  matrix4 =
+  matrix2 =
     fold(multiply, one, 2, 
       second(LSTM, St_c), 
       first(LSTM, Bt_phi));
-  matrix5 = fold(add, zero, 2, matrix3, matrix4);
-  push(LSTM, St_c, matrix5);
+  matrix3 = fold(add, zero, 2, matrix1, matrix2);
+  push(LSTM, St_c, matrix3);
 
-  //TEST 1:
+  // TEST 1:
   print_LSTM(LSTM);
-
-  // TEST 3a:
-  //matrix3 = fold(add, zero, 2, matrix1, matrix2);
-  //print_matrix(matrix3);
 
   // TEST 3:
   matrix1 = destroy_matrix(matrix1);
   matrix2 = destroy_matrix(matrix2);
-  matrix3 = destroy_matrix(matrix3);
-  matrix4 = destroy_matrix(matrix4);
-  //matrix5 = destroy_matrix(matrix5);
 
+  // TEST 1:
   LSTM = destroy_LSTM(LSTM);
   return 0;
 }
