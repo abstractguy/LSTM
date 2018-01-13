84d87d69965a1262cd97f07c9012879301a33402
Samuel Duclos <samuel@localhost.localdomain>
2018-01-12 21:43:46 -0500

    Debugging...

--- C/new/tests.c
+++ C/new/tests.c
@@ -9,6 +9,7 @@ int main(int argc, char *argv[]) {
   matrix *matrix2 = make_matrix(4, 1);
   matrix *matrix3 = NULL;
   matrix *matrix4 = NULL;
+  matrix *matrix5 = NULL;
   matrix_for_each(zero, matrix1);
   matrix_for_each(one, matrix2);
 
@@ -28,7 +29,8 @@ int main(int argc, char *argv[]) {
     fold(multiply, one, 2, 
       second(LSTM, St_c), 
       first(LSTM, Bt_phi));
-  push(LSTM, St_c, fold(add, zero, 2, matrix3, matrix4));
+  matrix5 = fold(add, zero, 2, matrix3, matrix4);
+  push(LSTM, St_c, matrix5);
 
   //TEST 1:
   print_LSTM(LSTM);
@@ -40,6 +42,7 @@ int main(int argc, char *argv[]) {
   // TEST 3:
   matrix3 = destroy_matrix(matrix3);
   matrix4 = destroy_matrix(matrix4);
+  matrix5 = destroy_matrix(matrix5);
 
   LSTM = destroy_LSTM(LSTM);
   return 0;
