4eab0012420f79f37c0bb9128c3493371247996a
Samuel Duclos <abstractguy@github.com>
2018-02-13 18:10:52 -0500

    Worked a bit on feedforward.c

--- C/feedforward.c
+++ C/feedforward.c
@@ -20,7 +20,7 @@ void feedforward_once(LSTM_type *LSTM) {
   // Input preactivations:
   push(LSTM, At_iota, 
     transpose(
-      fold(3, add, 
+      sum(3, 
         dot_product(
           transpose(first(LSTM, Xt_i)), 
           first(LSTM, Wi_iota)),
@@ -37,7 +37,7 @@ void feedforward_once(LSTM_type *LSTM) {
   // Forget preactivations:
   push(LSTM, At_phi, 
     transpose(
-      fold(3, add, 
+      sum(3, 
         dot_product(
           transpose(first(LSTM, Xt_i)), 
           first(LSTM, Wi_phi)),
@@ -52,25 +52,6 @@ void feedforward_once(LSTM_type *LSTM) {
   push(LSTM, Bt_phi, matrix_sigmoid(first(LSTM, At_phi)));
 
 /*
-  // Input activations:
-  push(LSTM, Bt_iota, matrix_sigmoid(first(LSTM, At_iota)));
-
-  // Forget preactivations:
-  push(LSTM, At_phi, 
-    sum(3, 
-      product(2, 
-        first(LSTM, Xt_i), 
-        first(LSTM, Wi_phi)), 
-      product(2, 
-        second(LSTM, Bt_h), 
-        first(LSTM, Wh_phi)), 
-      product(2, 
-        second(LSTM, St_c), 
-        first(LSTM, Wc_phi))));
-
-  // Forget activations:
-  push(LSTM, Bt_phi, matrix_sigmoid(first(LSTM, At_phi)));
-
   // Cell preactivations:
   push(LSTM, At_c, 
     sum(2, 
@@ -90,23 +71,25 @@ void feedforward_once(LSTM_type *LSTM) {
       fold(2, multiply, 
         second(LSTM, St_c), 
         first(LSTM, Bt_phi))));
+*/
 
   // Output preactivations:
   push(LSTM, At_omega, 
     sum(3, 
       product(2, 
-        first(LSTM, Xt_i), 
+        transpose(first(LSTM, Xt_i)), 
         first(LSTM, Wi_omega)), 
       product(2, 
-        second(LSTM, Bt_h), 
+        transpose(second(LSTM, Bt_h)), 
         first(LSTM, Wh_omega)), 
       product(2, 
-        first(LSTM, St_c), 
+        transpose(first(LSTM, St_c)), 
         first(LSTM, Wc_omega))));
 
   // Output activations:
   push(LSTM, Bt_omega, matrix_sigmoid(first(LSTM, At_omega)));
 
+/*
   // Cell outputs:
   push(LSTM, Bt_c, 
     fold(2, multiply, 
