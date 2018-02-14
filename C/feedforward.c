973eadfe98d4836d22a3225447c89a24720aea89
Samuel Duclos <abstractguy@github.com>
2018-02-14 18:40:35 -0500

    Finished (?) feedforward.c

--- C/feedforward.c
+++ C/feedforward.c
@@ -3,21 +3,6 @@
 
 void feedforward_once(LSTM_type *LSTM) {
   // Input preactivations:
-/*
-  push(LSTM, At_iota, 
-    sum(3, 
-      product(2, 
-        first(LSTM, Xt_i), 
-        first(LSTM, Wi_iota)), 
-      product(2, 
-        second(LSTM, Bt_h), 
-        first(LSTM, Wh_iota)), 
-      product(2, 
-        second(LSTM, St_c), 
-        first(LSTM, Wc_iota))));
-*/
-
-  // Input preactivations:
   push(LSTM, At_iota, 
     transpose(
       sum(3, 
@@ -51,49 +36,47 @@ void feedforward_once(LSTM_type *LSTM) {
   // Forget activations
   push(LSTM, Bt_phi, matrix_sigmoid(first(LSTM, At_phi)));
 
-/*
   // Cell preactivations:
   push(LSTM, At_c, 
-    sum(2, 
-      product(2, 
-        first(LSTM, Xt_i), 
-        first(LSTM, Wi_c)), 
-      product(2, 
-        second(LSTM, Bt_h), 
-        first(LSTM, Wh_c))));
+    transpose(
+      sum(2, 
+        dot_product(
+          transpose(first(LSTM, Xt_i)), 
+          first(LSTM, Wi_c)), 
+        dot_product(
+          transpose(second(LSTM, Bt_h)), 
+          first(LSTM, Wh_c)))));
 
   // Cell activations:
   push(LSTM, St_c, 
-    fold(2, add, 
-      fold(2, multiply, 
+    sum(2, 
+      product(2, 
         matrix_tanh(first(LSTM, At_c)), 
         first(LSTM, Bt_iota)), 
-      fold(2, multiply, 
+      product(2, 
         second(LSTM, St_c), 
         first(LSTM, Bt_phi))));
-*/
 
   // Output preactivations:
   push(LSTM, At_omega, 
-    sum(3, 
-      product(2, 
-        transpose(first(LSTM, Xt_i)), 
-        first(LSTM, Wi_omega)), 
-      product(2, 
-        transpose(second(LSTM, Bt_h)), 
-        first(LSTM, Wh_omega)), 
-      product(2, 
-        transpose(first(LSTM, St_c)), 
-        first(LSTM, Wc_omega))));
+   transpose(
+      sum(3, 
+        dot_product(
+          transpose(first(LSTM, Xt_i)), 
+          first(LSTM, Wi_omega)), 
+        dot_product(
+          transpose(second(LSTM, Bt_h)), 
+          first(LSTM, Wh_omega)), 
+        dot_product(
+          transpose(first(LSTM, St_c)), 
+          first(LSTM, Wc_omega)))));
 
   // Output activations:
   push(LSTM, Bt_omega, matrix_sigmoid(first(LSTM, At_omega)));
 
-/*
   // Cell outputs:
   push(LSTM, Bt_c, 
-    fold(2, multiply, 
+    product(2, 
       matrix_sigmoid(first(LSTM, St_c)), 
       first(LSTM, Bt_omega)));
-*/
 }
