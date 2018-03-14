e3bab9fcf149ca8ef02feebf92d205eab29c24fb
Samuel Duclos <samuel@localhost.localdomain>
2018-03-13 22:12:23 -0400

    Improved printing

--- printing.c
+++ printing.c
@@ -1,6 +1,15 @@
 // printing.c
 #include "printing.h"
 
+char *LSTM_string[52] = {
+  "Input", "Input_reversed", "Xt", "Xt_reversed", "Output", "Answer", "Yt",
+  "_Zt", "_It", "_Ft", "_Ot", "Zt", "It", "Ft", "Ot", "Ct", "Wz", "Wi",
+  "Wf", "Wo", "Rz", "Ri", "Rf", "Ro", "Pi", "Pf", "Po", "Bz", "Bi", "Bf",
+  "Bo", "DYt", "DOt", "DCt", "DFt", "DIt", "DZt", "DWz", "DWi", "DWf",
+  "DWo", "DRz", "DRi", "DRf", "DRo", "DPi", "DPf", "DPo", "DBz", "DBi",
+  "DBf", "DBo"
+};
+
 void print_matrix(matrix_type *matrix) {
   for (unsigned int row = 0; row < matrix->rows; row++) {
     for (unsigned int column = 0; column < matrix->columns; column++) {
@@ -11,7 +20,7 @@ void print_matrix(matrix_type *matrix) {
 
 void print_LSTM(LSTM_type *LSTM) {
   for (index_type tensor = 0; tensor < LSTM_SIZE; tensor++) {
-    printf("\nType %u:", tensor);
+    printf("\nType %s:", LSTM_string[tensor]);
     for (unsigned int time = 0; time < LSTM->tensor[tensor].time; time++) {
       printf("\nTime %u:\n", time);
       print_matrix(LSTM->tensor[tensor].matrix[time]);
