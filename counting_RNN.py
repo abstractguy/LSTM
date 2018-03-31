fb26416c6dd1f6480802be0bfd2163250713e4ba
Samuel Duclos <samuel@localhost.localdomain>
2018-03-31 19:27:48 -0400

    Simplified counting_RNN.py

--- counting_RNN.py
+++ counting_RNN.py
@@ -7,8 +7,7 @@ hidden_synapses = 2 * np.random.random((16, 16)) - 1
 output_synapses = 2 * np.random.random((16, 2))  - 1
 learning_rate, outputs = 0.1, np.zeros_like(answers)
 for epoch in range(10000):
-	hidden_layers, output_layers, output_errors = list(), list(), list()
-	hidden_layers.append(np.zeros(16))
+	hidden_layers, output_layers, output_errors = [np.zeros(16)], [], []
 	input_synapses_update  = np.zeros_like(input_synapses)
 	hidden_synapses_update = np.zeros_like(hidden_synapses)
 	output_synapses_update = np.zeros_like(output_synapses)
@@ -20,12 +19,13 @@ for epoch in range(10000):
 		output_layers.append(copy.deepcopy(output_layer))
 	next_hidden_error = np.zeros(16)
 	for time in range(4):
-		hidden_error = (next_hidden_error.dot(hidden_synapses.T) + output_errors[-time - 1].dot(output_synapses.T)) * (hidden_layers[-time - 1] * (1 - hidden_layers[-time - 1]))
+		hidden_error = (next_hidden_error.dot(hidden_synapses.T) + output_errors[-time - 1].dot(output_synapses.T)) \
+				* (hidden_layers[-time - 1] * (1 - hidden_layers[-time - 1]))
 		output_synapses_update += np.atleast_2d(hidden_layers[-time - 1]).T.dot(output_errors[-time - 1])
 		hidden_synapses_update += np.atleast_2d(hidden_layers[-time - 2]).T.dot(hidden_error)
 		input_synapses_update  += inputs[-time - 1].T.dot(hidden_error)
 		next_hidden_error = hidden_error
-	input_synapses  += input_synapses_update *  learning_rate
+	input_synapses  += input_synapses_update  * learning_rate
 	hidden_synapses += hidden_synapses_update * learning_rate
 	output_synapses += output_synapses_update * learning_rate
 
