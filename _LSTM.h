6e5e9705571dd34971e434b1c257db975ce1f3ba
Samuel Duclos <samuel@localhost.localdomain>
2018-04-01 13:50:43 -0400

    LSTM working (underscored versions)less _output.txt  Could simplify some stuff though...

--- _LSTM.h
+++ _LSTM.h
@@ -3,9 +3,9 @@
   #define _LSTM_H
   #include "matrix.h"
   //#include <time.h>
-  #define TIME_SIZE            4
+  #define TIME_SIZE            6
   #define BATCH_SIZE           1
-  #define WORD_SIZE            2
+  #define WORD_SIZE            4
   #define HIDDEN_SIZE          16
   #define LSTM_BEGIN           0
   #define LSTM_SIZE            37
